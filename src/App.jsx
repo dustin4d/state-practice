1ee541239e09023ccbfdd32bd21b54c344c169ff
Dustin H <Dustinholder14@yahoo.com>
2025-04-15 16:38:35 -0500

    Left and right clicks managed in state object

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import { useState } from 'react'
 
-// Concise arrow function since only one return statement
-// Destructuring props
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-	const [counter, setCounter] = useState(0)
 
-	const increaseByOne = () => setCounter(counter + 1)
-	const increaseByThree = () => setCounter(counter + 3)
-	const increaseByFive = () => setCounter(counter + 5)
-	const reset = () => setCounter(0)
+	const [clicks, setClicks] = useState({left: 0, right: 0})
+
+	const handleLeftClick = () => {
+		const newClicks = {
+			left: clicks.left + 1,
+			right: clicks.right + 0
+		}
+		setClicks(newClicks)
+	}
+
+	const handleRightClick = () => {
+		const newClicks = {
+			left: clicks.left + 0,
+			right: clicks.right + 1,
+		}
+		setClicks(newClicks)
+	}
 
 	return (
 	<div>
-		<Display counter={counter}/>
-		<Button onClick={increaseByOne} text="+1"></Button>
-		<Button onClick={increaseByThree} text="+3"></Button>
-		<Button onClick={increaseByFive} text="+5"></Button>
-		<Button onClick={reset} text="Reset"></Button>
+		{clicks.left}
+		<button onClick={handleLeftClick}>Left</button>
+		<button onClick={handleRightClick}>Right</button>
+		{clicks.right}
 	</div>
 	)
 }
