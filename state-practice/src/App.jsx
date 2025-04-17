8e579836b56767cc257c7c6aed9ae760ff4bd689
Dustin H <Dustinholder14@yahoo.com>
2025-04-17 15:36:38 -0500

    Complex state and debugging

--- state-practice/src/App.jsx
+++ state-practice/src/App.jsx
@@ -1,50 +1,27 @@
 import { useState } from 'react'
 
-const History = (props) => {
-	if (props.allClicks.length === 0) {
-		return (
-			<div>Press the buttons to track clicks</div>
-		)
-	}
-
-	return (
-		<div>
-			History: {props.allClicks}
-		</div>
-	)
-}
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>	
-
 const App = () => {
+  const [value, setValue] = useState(10)
 
-	const [left, setLeft] = useState(0)
-	const [right, setRight] = useState(0)
-	const [allClicks, setAll] = useState([])
-	const [total, setTotal] = useState(0)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
 
-	const handleLeftClick = () => {
-		setAll(allClicks.concat('L'))	
-		const updatedLeft = left + 1 // Pass updated value to state before rerender.
-		setLeft(updatedLeft)
-		setTotal(updatedLeft + right)
-	}
-
-	const handleRightClick = () => {
-		setAll(allClicks.concat('R'))
-		const updatedRight = right + 1
-		setRight(updatedRight)
-		setTotal(updatedRight + left)
-	}
-
-	return (
-	<div>
-		<Button onClick={handleLeftClick} text="Left" />
-		<Button onClick={handleRightClick} text="Right" />
-		<History allClicks={allClicks} />
-		<p>Total: {total}</p>
-	</div>
-	)
+  return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
+    </div>
+  )
 }
 
 export default App
