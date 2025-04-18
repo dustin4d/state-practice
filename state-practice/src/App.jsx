e2b8a77f8b8584abfd12d3724451346fb6d3b5d2
Dustin H <Dustinholder14@yahoo.com>
2025-04-18 15:06:29 -0500

    Do not define components inside other components

--- state-practice/src/App.jsx
+++ state-practice/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
+import {useState} from 'react'
+
+// This is the right place to define a component
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
+
+const Display = props => <div>{props.value}</div>
 
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const setToValue = (newValue) => {
+  const setToValue = newValue => {
     console.log('value now', newValue)
     setValue(newValue)
   }
 
   return (
     <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
+
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
