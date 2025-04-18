2152ee0d775ec37874fd0ee5cf079e8c0b0d16d2
Dustin H <dustinholder14@yahoo.com>
2025-04-17 20:20:17 -0500

    really condensed event handler definition

--- complex-state/src/App.jsx
+++ complex-state/src/App.jsx
@@ -3,16 +3,14 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10) // init state at 10
 
-  // Event handler stuff
-  const handleClick = () => {
-    return (() => setValue(0))
-  }
+  // Event handler stuff -- condensed arrow function
+  const handleClick = () => setValue(0)
 
   return ( 
-
     <div>
       {value}
-      <button onClick={handleClick()}>reset to zero</button>
+      <br/>
+      <button onClick={handleClick}>reset to zero</button>
     </div>
   )
 }
